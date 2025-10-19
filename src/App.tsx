8e1da0cf9e2f6260f50efc9c89ce2a1b7824df5a
Marcos Gomez Alvarez <83846095+marcg0128@users.noreply.github.com>
2025-10-20 00:51:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import NavBar from "./components/navbar.tsx";
+
+
+function App() {
+
+    return (
+        <>
+            <NavBar/>
+
+            <div className="justify-center flex w-full h-full mt-[15%]">
+                <h1 className={`text-3xl md:text-9xl ${
+                    ''
+                }`}>Marcos Gomez Alvarez</h1>
+
+            </div>
+            <p className="mt-[1300px]">
+                asdasd
+            </p>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,20 @@
+export default function NavBar() {
+    return (
+        <div className="flex justify-center items-center align-top h-16 w-full fixed top-0 left-0 z-50 animate-fade">
+            <nav className="w-1/2 flex items-center justify-evenly gap-x-28 h-20 mt-20 rounded-4xl px-16 py-12 outline-1 outline-[#606060]">
+                <div>
+sadasdsadsad
+                </div>
+                <div className="self-end-safe">
+                    <div>Developer · Innovator · Tech Enthusiast</div>
+                </div>
+                <div>
+                    <button className="bg-[#0F171F] p-4 px-6 rounded-4xl">
+                        Contact Me
+                    </button>
+                </div>
+            </nav>
+        </div>
+
+    );
+}
